d9139fe113f1c8bed94b0b265f1b8e1d8b4a657b
Dave Lewis <siu06dl@gmail.com>
2024-07-19 18:46:19 +0100

    fix: growl shouldn't be continuous

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -5,11 +5,13 @@ namespace Assets.Enemy
 {
     public class Enemy : MonoBehaviour
     {
+        public float GrowlBackOff = 5f;
         public Rigidbody2D Rigidbody;
         public float Speed = 1.0f;
         public GameObject Target;
 
         [SerializeField] private List<AudioClip> Growls;
+        private float _growlCooldown;
 
         private void Update()
         {
@@ -21,7 +23,8 @@ namespace Assets.Enemy
             MoveTowardsTarget(direction);
 
             if (direction.magnitude < 5f)
-                SoundEffectsManager.instance.PlayRandomSoundEffect(Growls, transform, 1f);
+                Growl();
+            // TODO consider different effects when in really close.
         }
 
         private void MoveTowardsTarget(Vector3 direction) =>
@@ -29,5 +32,15 @@ namespace Assets.Enemy
 
         private Vector3 GetDirection(Vector3 targetPosition, Vector3 position) =>
             targetPosition - position;
+
+        private void Growl()
+        {
+            if (_growlCooldown == 0)
+                _growlCooldown = SoundEffectsManager.instance.PlayRandomSoundEffect(Growls, transform, 1f) + GrowlBackOff;
+            else if (_growlCooldown > 0)
+                _growlCooldown -= Time.deltaTime;
+            else if (_growlCooldown < 0)
+                _growlCooldown = 0;
+        }
     }
 }
