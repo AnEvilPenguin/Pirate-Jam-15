71f1176c39c7ebc0c4ad3bacc1b9b4d526320bfb
Dave Lewis <siu06dl@gmail.com>
2024-07-19 17:57:48 +0100

    feat: follow target

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -4,5 +4,18 @@ namespace Assets.Enemy
 {
     public class Enemy : MonoBehaviour
     {
+        public Rigidbody2D Rigidbody;
+        public float Speed = 1.0f;
+        public GameObject Target;
+
+        private void Update()
+        {
+            var targetLocation = Target.transform.position;
+            var origin = gameObject.transform.position;
+
+            var direction = targetLocation - origin;
+
+            Rigidbody.velocity = direction.normalized * Speed;
+        }
     }
 }
