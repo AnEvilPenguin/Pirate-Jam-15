cdacc9bc5d94f27c1ecd66724ce9ae86f8ed8b8c
Dave Lewis <siu06dl@gmail.com>
2024-07-19 18:28:48 +0100

    refactor: simplify movement functionality

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -10,17 +10,19 @@ namespace Assets.Enemy
 
         private void Update()
         {
-            MoveTowardsTarget();
-        }
+            var targetPosition = Target.transform.position;
+            var position = gameObject.transform.position;
 
-        private void MoveTowardsTarget()
-        {
-            var targetLocation = Target.transform.position;
-            var origin = gameObject.transform.position;
+            var direction = GetDirection(targetPosition, position);
+
+            MoveTowardsTarget(direction);
 
             var direction = targetLocation - origin;
 
+        private void MoveTowardsTarget(Vector3 direction) =>
             Rigidbody.velocity = direction.normalized * Speed;
-        }
+
+        private Vector3 GetDirection(Vector3 targetPosition, Vector3 position) =>
+            targetPosition - position;
     }
 }
