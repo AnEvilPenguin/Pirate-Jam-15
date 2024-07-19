cb7b9a6c9f8249654e52941281d98695008130d7
mrAlienHead <ray_ray_jackson@hotmail.co.uk>
2024-07-19 16:52:53 +0100

    MovementController & Basic Animation
    Added a movement controller to replace auto-generated code and added in some basic animations.

---
+++ Assets/Enemy/Enemy.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Assets.Enemy
+{
+    public class Enemy : MonoBehaviour
+    {
+    }
+}
---
+++ Assets/Player/PlayerEnums.cs
@@ -0,0 +1,22 @@
+﻿namespace Assets.Player
+{
+    public enum PlayerDirection
+    {
+        North,
+        NorthEast,
+        East,
+        SouthEast,
+        South,
+        SouthWest,
+        West,
+        NorthWest
+    }
+
+    public enum PlayerState
+    {
+        Dead,
+        Idle,
+        Moving
+    }
+}
+
